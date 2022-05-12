e5cb89ecc862f95b1f253a73a6768f101890dfdb
ahmed <ahmedismailm85@gmail.com>
2022-05-12 14:33:44 +0200

    fix: bug using old req.post -> req.body

--- src/vwn/tests.py
+++ src/vwn/tests.py
@@ -1,3 +1,12 @@
+import json
 from django.test import TestCase
 
-# Create your tests here.
+class IndexTest(TestCase):
+    def test_post(self):
+            data = {"hours":1}
+            json_data = json.dumps(data)
+            response = self.client.post("/",data=json_data,  content_type='application/json')
+    #         print(response.content)
+    # def test_get(self):
+    #         response = self.client.get("/")
+    #         print(response.content)
--- src/vwn/views.py
+++ src/vwn/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from vwn.models import Exercise
-from django.views.decorators.csrf import csrf_exempt
-
-# Create your views here.
-@csrf_exempt 
+# from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.http import require_http_methods
+# # Create your views here.
+# @csrf_exempt 
+# @require_http_methods(["POST"])
 def index(request):
     if request.method == 'GET':
         return HttpResponse('Please provide exerciseName(string),hours(tinyint),mins(tinyint))', content_type='application/json')
     if request.method == 'POST':
-        print(request.POST.get('hours'))
+        print(request.body, "after printing req" )
+        # for k , v in request.POST.items():
+        #     print(k, v)
         # xrsyz = Exercise.objects.create(name=request.POST.get('exerciseName'), hours=request.POST.get('hours'),
         # min=request.POST.get('mins'))
         # print(xrsyz)
