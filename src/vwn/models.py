c1988bcb4f53f5c6ddd30899201133f82e61ff8a
ahmed <ahmedismailm85@gmail.com>
2022-05-12 11:34:37 +0200

    init: start model

--- src/vwn/models.py
+++ src/vwn/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=255, null=False, unique=True)
+    hours = models.SmallIntegerField()
+    min = models.SmallIntegerField()
+    def __str__(self):
+        return self.name.capitalize()
+
--- src/vwn/views.py
+++ src/vwn/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from vwn.models import Exercise
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
+@csrf_exempt 
 def index(request):
-    return HttpResponse('hello', content_type='application/json')
+    if request.method == 'GET':
+        return HttpResponse('Please provide exerciseName(string),hours(tinyint),mins(tinyint))', content_type='application/json')
+    if request.method == 'POST':
+        print(request.POST.get('hours'))
+        # xrsyz = Exercise.objects.create(name=request.POST.get('exerciseName'), hours=request.POST.get('hours'),
+        # min=request.POST.get('mins'))
+        # print(xrsyz)
+        return HttpResponse('All good', content_type='application/json')
