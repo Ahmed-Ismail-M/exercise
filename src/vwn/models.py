83340b33343daa1ffa991e54ba72d779804a2369
ahmed <ahmedismailm85@gmail.com>
2022-05-12 21:03:17 +0200

    add validators

--- src/vwn/models.py
+++ src/vwn/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
 class Exercise(models.Model):
     name = models.CharField(max_length=255, null=False)
-    hours = models.SmallIntegerField()
-    min = models.SmallIntegerField()
+    hours = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(24)])
+    min = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(60)])
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
